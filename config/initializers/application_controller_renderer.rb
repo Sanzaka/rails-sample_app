02664f8fec23b0b8dcdef9f34f9d8eb4c2e0b42f
Sanzaka <equation0815@icloud.com>
2021-06-23 05:03:53 +0000

    [end]1-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f3c37bc22a907590b68d11809e92c89b7ae5f5caebe3fa4402ff11b9aa6220bd9e72dfb920573e15d4e49d7fed3fb84ad864e7a545dfb823a67ee3defc963eca'
